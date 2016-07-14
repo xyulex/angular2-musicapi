c4c8e8b257906957db4f8b2c5a4fe1138c76890e
xyulex <raulmartinez911@hotmail.com>
2016-07-14 12:42:53 +0200

    CORS: Delete method allowed

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
